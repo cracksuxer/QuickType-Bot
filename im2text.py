3dfd5aa6dfbf7b25558dfbc9b9a054cb6c65b50e
Julio <sopliac@hotmail.es>
2023-11-21 09:56:25 +0000

    Auto stash before merge of "im2text-dev" and "origin/im2text-dev"

--- im2text.py
+++ im2text.py
@@ -46,8 +46,8 @@ def preprocess_region(region, region_color, gray_image):
         # Invert colors if the text is white (light on dark)
         region_of_interest = cv2.bitwise_not(expanded_region_of_interest)
         
-        # plt.imshow(cv2.cvtColor(region_of_interest, cv2.COLOR_BGR2RGB))
-        # plt.show()
+        plt.imshow(cv2.cvtColor(region_of_interest, cv2.COLOR_BGR2RGB))
+        plt.show()
         return region_of_interest
     else:
         # Threshold the image to create a mask for the gray text
@@ -65,8 +65,8 @@ def preprocess_region(region, region_color, gray_image):
         region_of_interest = cv2.bitwise_and(expanded_region_of_interest, expanded_region_of_interest, mask=mask)
 
         # Show the region of interest
-        # plt.imshow(cv2.cvtColor(region_of_interest, cv2.COLOR_BGR2RGB))
-        # plt.show()
+        plt.imshow(cv2.cvtColor(region_of_interest, cv2.COLOR_BGR2RGB))
+        plt.show()
         
         # Now, let's increase the contrast to make the gray text white
         # You can adjust the contrast level as needed
@@ -82,16 +82,18 @@ def segment_text_regions_with_dilation(binary_image, kernel_size=(10,10)):
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, kernel_size)
     dilated_image = cv2.dilate(binary_image, kernel, iterations=1)
     
+    # Show the dilated image
+    plt.imshow(cv2.cvtColor(dilated_image, cv2.COLOR_BGR2RGB))
+    plt.show()
+
     contours, _ = cv2.findContours(dilated_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     text_regions = [cv2.boundingRect(cnt) for cnt in contours]
 
     return text_regions
 
 def sort_text_regions(text_regions):
-    # First, sort by the y-coordinate to ensure we process top-to-bottom
     text_regions.sort(key=lambda x: x[1])
     
-    # Next, we'll cluster the y-coordinates to find lines of text
     line_clusters = {}
     line_height_threshold = 10  # This threshold can be adjusted
 
@@ -123,10 +125,13 @@ def get_image_text_and_colors(image_path: str = './test2.png') -> list[tuple[str
     plt.imshow(cv2.cvtColor(gray_image, cv2.COLOR_BGR2RGB))
     plt.show()
 
-    # Threshold the image to create a binary image
-    # Assuming you have a function to find a suitable threshold value
-    threshold_value = 90  # This should be determined by your determine_threshold_value function
+    # Threshold for the gray and white text regions
+    threshold_value = 90
     _, binary_image = cv2.threshold(gray_image, threshold_value, 255, cv2.THRESH_BINARY)
+
+    # Show the binary image
+    plt.imshow(binary_image, cmap='gray')
+    plt.show()
 
     # Segment text regions
     text_regions = segment_text_regions_with_dilation(binary_image)
@@ -147,33 +152,10 @@ def get_image_text_and_colors(image_path: str = './test2.png') -> list[tuple[str
 
     for region in sorted_regions:
         region_color = determine_region_color(region, gray_image)
-
-        # crop that region from the grayscale image and show it
-        x, y, w, h = region
-        border_size = 2  # This will add 5 pixels on each side
-
-        # Make sure that we don't go out of the image boundaries
-        x_start = max(x - border_size, 0)
-        y_start = max(y - border_size, 0)
-        x_end = min(x + w + border_size, gray_image.shape[1])
-        y_end = min(y + h + border_size, gray_image.shape[0])
-
-        # Extract the region of interest with the additional border
-        expanded_region_of_interest = gray_image[y_start:y_end, x_start:x_end]
-        # plt.imshow(cv2.cvtColor(expanded_region_of_interest, cv2.COLOR_BGR2RGB))
-        # plt.show()
-
         preprocessed_region = preprocess_region(region, region_color, gray_image)
-        
-        # Extract the region for OCR
-        x, y, w, h = region
-        roi = preprocessed_region
 
-        
-        # Perform OCR on the preprocessed region of interest
-        text: str = pytesseract.image_to_string(roi, config='--psm 7', lang='spa')
+        text = pytesseract.image_to_string(preprocessed_region, config='--psm 7', lang='spa')
         text = str(text).replace('\n', '')
-        console.print(text)
         text_results.append((text, region_color))
 
 
