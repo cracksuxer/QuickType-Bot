0d4673eae0ceefd5e1c63a0b38f0064f82dcc81f
Daniel Mendez <alu0101391793@ull.edu.es>
2023-12-03 21:46:15 +0000

    [FEAT] Clean doc files and Update im2text.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from rich.console import Console
+from quicktype.im2text import *
 
 console = Console()
 
+
 def run():
     console.print("Hello, [bold magenta]World[/bold magenta]!", soft_wrap=True)
+    get_image_text_and_colors("img/test.png")
