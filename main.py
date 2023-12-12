3b965c4a8648436595bd012bccb887b2919784b1
Daniel Mendez <alu0101391793@ull.edu.es>
2023-12-12 10:06:41 +0000

    [FEAT] Create GUI module and add PySimpleGui to poetry

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from rich.console import Console
-from quicktype.im2text import *
+from quicktype.im2text import get_image_text_and_colors
+from quicktype.gui import start_gui
 
 console = Console()
 
 
 def run():
+    start_gui()
     console.print("Hello, [bold magenta]World[/bold magenta]!", soft_wrap=True)
     get_image_text_and_colors("img/test.png")
